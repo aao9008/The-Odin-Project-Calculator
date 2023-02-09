0719dda6bf7ff5be594a30e9ecc43f2c2b20730b
Alfredo Ormeno <ormenoandres13@gmail.com>
2023-02-08 20:21:25 -0500

    Add ability to display user input on the screen
    This commit implements the ability to display input from the user on the calculator screen.
    
    This functionality is implemented through the "getValueOnScreen" function.

--- javascript.js
+++ javascript.js
@@ -16,5 +16,23 @@ const numbers = document.querySelector('.calculator-buttons');
 
 // This function will display numbers on  the screen
 function getNumberOnScreen(e){
-    if (e.target.class != operand)
+    // Check that button is a number or decimal point
+    if (!e.target.classList.contains("operand"))
+        return;
+
+    // Add value to screen if there are less than 12 characters on the screen
+    if (screen.textContent.length < 11){
+        screen.append(e.target.value);
+
+        // Disable decimal after one press
+        if (e.target.value === "."){
+            e.target.disabled = true;
+        }
+    }
+    else{
+        return; // Don not add value to screen if screen is full
+    }
 }
+
+// Listen for Number input
+numbers.addEventListener('click', getNumberOnScreen);
